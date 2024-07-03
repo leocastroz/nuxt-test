b757f1c6689e58d51dc3334bfd1a87fcfaa83f9f
leocastroz <castroz.de@gmail.com>
2024-07-02 23:59:58 -0300

    fix: adjustment NuxtConfif#01

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_BASE_URL
+    }
+  },
   devtools: { enabled: true },
   css: ['~/src/css/default.css'],
   plugins: ['~/plugins/axios.ts', '~/plugins/pinia.ts'],
